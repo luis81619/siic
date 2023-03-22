db7d994c0ff743f0715f1bbcdc9ec0680a8de393
luis81619 <ferluisrv25@gmail.com>
2023-03-22 15:58:34 -0600

    ready

--- api_siic/routes/api.php
+++ api_siic/routes/api.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+*/
+
+Route::group(['middleware' => 'api'], function($router) {
+    Route::post('/register', [JWTController::class, 'register']);
+    Route::post('/login', [JWTController::class, 'login']);
+    Route::post('/logout', [JWTController::class, 'logout']);
+    Route::post('/refresh', [JWTController::class, 'refresh']);
+    Route::post('/profile', [JWTController::class, 'profile']);
 });
