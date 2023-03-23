d5e911f4a60022c5c3ace13b6df329ebf329dd56
luis81619 <ferluisrv25@gmail.com>
2023-03-23 16:02:51 -0600

    login por post

--- api_siic/app/Http/Controllers/JWTController.php
+++ api_siic/app/Http/Controllers/JWTController.php
@@ -30,7 +30,7 @@ class JWTController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|min:2|max:100',
             'email' => 'required|string|email|max:100|unique:users',
-            'password' => 'required|string|confirmed|min:6',
+            'password' => 'required|string|min:6',
         ]);
 
         if($validator->fails()) {
@@ -65,7 +65,7 @@ class JWTController extends Controller
             return response()->json($validator->errors(), 422);
         }
 
-        if (!$token = auth()->attempt($validator->validated())) {
+        if (!$token = auth('api')->attempt($validator->validated())) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
@@ -79,7 +79,7 @@ class JWTController extends Controller
      */
     public function logout()
     {
-        auth()->logout();
+        auth('api')->logout();
 
         return response()->json(['message' => 'User successfully logged out.']);
     }
@@ -91,7 +91,7 @@ class JWTController extends Controller
      */
     public function refresh()
     {
-        return $this->respondWithToken(auth()->refresh());
+        return $this->respondWithToken(auth('api')->refresh());
     }
 
     /**
@@ -116,7 +116,8 @@ class JWTController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            'expires_in' => auth('api')->factory()->getTTL() * 60,
+            "user" => auth('api')->user(),
         ]);
     }
 }
